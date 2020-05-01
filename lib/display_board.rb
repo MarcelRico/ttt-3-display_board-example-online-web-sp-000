eae9549c22291d221e68d821fe286b98cd27c599
Marcel Rico <personal.mr4@gmail.com>
2020-05-01 00:02:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
 def display_board
-  i = 0
-  while i < 4
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------
   puts "   |   |   "
 end
